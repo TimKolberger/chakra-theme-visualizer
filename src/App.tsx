f5cc462ae00a8524956a346a0dd1725772be52c7
Tim Kolberger <tim@kolberger.eu>
2021-02-14 17:26:05 +0100

    :sparkles: Add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ThemeVisualizer } from "./components/ThemeVisualizer";
 import { theme } from "./theme";
-import { BrowserRouter, Redirect, Route } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { NotFoundPage } from "./components/NotFoundPage";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <BrowserRouter basename="/">
-      <Route path="/" exact render={() => <Redirect to="/theme/fonts" />} />
-      <Route path="/theme/:section" exact>
-        <ThemeVisualizer theme={theme} />
-      </Route>
-      <Route path="*" exact>
-        Not found
-      </Route>
+      <Switch>
+        <Route path="/" exact render={() => <Redirect to="/theme/fonts" />} />
+        <Route path="/theme/:section" exact>
+          <ThemeVisualizer theme={theme} />
+        </Route>
+        <Route path="*" exact component={NotFoundPage} />
+      </Switch>
     </BrowserRouter>
   </ChakraProvider>
 );
